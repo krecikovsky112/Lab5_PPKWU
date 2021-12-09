4adb5739033a42eb276697d9164665dd8984c88a
krecikovsky112 <krecikovsky112@gmail.com>
2021-12-09 12:50:01 +0100

    Config project and add Model Specialist

---
+++ src/main/java/com/example/demo/Specialist.java
@@ -0,0 +1,78 @@
+package com.example.demo;
+
+public class Specialist {
+    private String name;
+    private String image;
+    private String phone;
+    private String email;
+    private String website;
+    private String street;
+    private String city;
+    private String postalCode;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+
+}
